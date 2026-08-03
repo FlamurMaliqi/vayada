a56eaa1ad2abfb9ed1dbbe5baa7d68cbdeec78b3
Flamur Maliqi <flamur.maliqi2811@gmail.com>
2026-08-03 17:16:57 +0200

    feat(finance): derive method payment readiness

--- packages/domain-finance/src/index.ts
+++ packages/domain-finance/src/index.ts
@@ -25,6 +25,7 @@
 import { createHash } from "node:crypto";
 
 export * from "./paymentReadiness.js";
+export * from "./paymentReadinessSnapshot.js";
 
 // ---------------------------------------------------------------------------
 // Scalar aliases
