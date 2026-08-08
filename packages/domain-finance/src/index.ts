3702c0c51268556a61103cc378423d64e469426d
Flamur Maliqi <flamur.maliqi2811@gmail.com>
2026-08-08 15:26:12 +0200

    feat(finance): define expense API contracts (VAY-1209)

--- packages/domain-finance/src/index.ts
+++ packages/domain-finance/src/index.ts
@@ -32,6 +32,7 @@ export * from "./subscriptions.js";
 export * from "./platformAffiliatePayouts.js";
 export * from "./affiliateCommission.js";
 export * from "./otaCommissionRules.js";
+export * from "./financialExpenses.js";
 
 // ---------------------------------------------------------------------------
 // Scalar aliases
